c710284e4d967f6a33a7ae10c2b0956215e682e4
Matt Graham <matthew.m.graham@gmail.com>
2016-07-18 19:03:29 +0100

    Adding initial test suite.

---
+++ revrng/tests/test_reversible_random_state.py
@@ -0,0 +1,137 @@
+import numpy as np
+from revrng.numpy_wrapper import ReversibleRandomState
+
+
+SEED = 12345
+N_ITER = 50
+IN_RANGE_SAMPLES = 10000
+SHAPES = [2, (1,), (5, 4), (3, 2, 1, 2)]
+
+
+def test_shape():
+    state = ReversibleRandomState(SEED)
+    for shape in SHAPES:
+        # ndarray shape always tuple even if integer specified
+        tuple_shape = shape if isinstance(shape, tuple) else (shape,)
+        samples = state.random_integers(shape)
+        assert samples.shape == tuple_shape, (
+            'random_integers shape mismatch: should be {0} actually {1}'
+            .format(tuple_shape, samples.shape)
+        )
+        samples = state.standard_uniform(shape)
+        assert samples.shape == tuple_shape, (
+            'standard_uniform shape mismatch: should be {0} actually {1}'
+            .format(tuple_shape, samples.shape)
+        )
+        samples = state.standard_normal(shape)
+        assert samples.shape == tuple_shape, (
+            'standard_normal shape mismatch: should be {0} actually {1}'
+            .format(tuple_shape, samples.shape)
+        )
+
+
+def test_dtype():
+    state = ReversibleRandomState(SEED)
+    shape = (5, 4)
+    samples = state.random_integers(shape)
+    assert samples.dtype == np.uint64, (
+        'random_integers dtype mismatch: should be uint64 actually {0}'
+        .format(samples.dtype)
+    )
+    samples = state.standard_uniform(shape)
+    assert samples.dtype == np.float64, (
+        'standard_uniform dtype mismatch: should be float64 actually {0}'
+        .format(samples.dtype)
+    )
+    samples = state.standard_normal(shape)
+    assert samples.dtype == np.float64, (
+        'standard_normal dtype mismatch: should be float64 actually {0}'
+        .format(samples.dtype)
+    )
+
+
+def test_reversibility_random_integers():
+    state = ReversibleRandomState(SEED)
+    samples_fwd = []
+    for i in range(N_ITER):
+        samples_fwd.append(state.random_integers(i + 1))
+    state.reverse()
+    for i in range(N_ITER - 1, -1, -1):
+        sample_fwd = samples_fwd.pop(-1)
+        sample_bwd = state.random_integers(i + 1)
+        assert np.all(sample_fwd == sample_bwd), (
+            'Incorrect reversed random_integers samples, expected {0} got {1}'
+            .format(sample_fwd, sample_bwd)
+        )
+
+
+def test_reversibility_standard_uniform():
+    state = ReversibleRandomState(SEED)
+    samples_fwd = []
+    for i in range(N_ITER):
+        samples_fwd.append(state.standard_uniform(i + 1))
+    state.reverse()
+    for i in range(N_ITER - 1, -1, -1):
+        sample_fwd = samples_fwd.pop(-1)
+        sample_bwd = state.standard_uniform(i + 1)
+        assert np.all(sample_fwd == sample_bwd), (
+            'Incorrect reversed standard_uniform samples, expected {0} got {1}'
+            .format(sample_fwd, sample_bwd)
+        )
+
+
+def test_reversibility_standard_normal():
+    state = ReversibleRandomState(SEED)
+    samples_fwd = []
+    for i in range(N_ITER):
+        samples_fwd.append(state.standard_normal(i + 1))
+    state.reverse()
+    for i in range(N_ITER - 1, -1, -1):
+        sample_fwd = samples_fwd.pop(-1)
+        sample_bwd = state.standard_normal(i + 1)
+        assert np.all(sample_fwd == sample_bwd), (
+            'Incorrect reversed standard_normal samples, expected {0} got {1}'
+            .format(sample_fwd, sample_bwd)
+        )
+
+
+def test_reversibility_mixed():
+    state = ReversibleRandomState(SEED)
+    samples_fwd = []
+    for i in range(N_ITER):
+        samples_fwd.append(state.random_integers(i + 1))
+        samples_fwd.append(state.standard_uniform(i + 1))
+        samples_fwd.append(state.standard_normal(i + 1))
+    state.reverse()
+    for i in range(N_ITER - 1, -1, -1):
+        # sample in reverse order
+        sample_fwd = samples_fwd.pop(-1)
+        sample_bwd = state.standard_normal(i + 1)
+        assert np.all(sample_fwd == sample_bwd), (
+            'Incorrect reversed standard_normal samples, expected {0} got {1}'
+            .format(sample_fwd, sample_bwd)
+        )
+        sample_fwd = samples_fwd.pop(-1)
+        sample_bwd = state.standard_uniform(i + 1)
+        assert np.all(sample_fwd == sample_bwd), (
+            'Incorrect reversed standard_uniform samples, expected {0} got {1}'
+            .format(sample_fwd, sample_bwd)
+        )
+        sample_fwd = samples_fwd.pop(-1)
+        sample_bwd = state.random_integers(i + 1)
+        assert np.all(sample_fwd == sample_bwd), (
+            'Incorrect reversed random_integers samples, expected {0} got {1}'
+            .format(sample_fwd, sample_bwd)
+        )
+
+
+def test_random_integers_in_range():
+    state = ReversibleRandomState(SEED)
+    samples = state.random_integers(IN_RANGE_SAMPLES)
+    assert np.all(samples >= 0) and np.all(samples < 2**32)
+
+
+def test_standard_uniform_in_range():
+    state = ReversibleRandomState(SEED)
+    samples = state.standard_uniform(IN_RANGE_SAMPLES)
+    assert np.all(samples >= 0.) and np.all(samples < 1.)
